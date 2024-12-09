556ab0e797b31837ac278cc68e6aee2a6dbce215
ZeroDeDragons <162035031+ZeroDeDragons@users.noreply.github.com>
2024-12-09 08:56:30 +0000

    Update inimigos.js
    caminho absoluto

--- componentes/inimigos.js
+++ componentes/inimigos.js
@@ -9,7 +9,7 @@ export class Canhao
     preload() 
     {
         this.ObjBala.preload();
-        this.cenario.load.image("Canhao", "../assets/canhao.png");
+        this.cenario.load.image("Canhao", "/assets2/canhao.png");
     }
 
     group()
